42052abeaceb269f08433518c9084b78733f6f30
Kleberson Canuto <klebersoncanuto@gmail.com>
2025-03-14 17:04:12 -0300

    Add ultraocr client

---
+++ ultraocr/Client.cs
@@ -0,0 +1,98 @@
+﻿namespace ultraocr;
+
+class Client
+{
+    private string ClientID { get; set; }
+    private string ClientSecret { get; set; }
+    private string BaseUrl { get; set; }
+    private string AuthBaseUrl { get; set; }
+    private string Token { get; set; }
+    private DateTime ExpiresAt { get; set; }
+    private long Expires { get; set; }
+    private long Timeout { get; set; }
+    private long Interval { get; set; }
+    private bool AutoRefresh { get; set; }
+    private HttpClient HttpClient { get; set; }
+
+    public Client() {
+        AuthBaseUrl = Constants.AUTH_BASE_URL;
+        BaseUrl = Constants.BASE_URL;
+        Timeout = Constants.API_TIMEOUT;
+        Interval = Constants.POOLING_INTERVAL;
+        Expires = Constants.DEFAULT_EXPIRATION_TIME;
+        AutoRefresh = false;
+        ClientID = "";
+        ClientSecret = "";
+        Token = "";
+        ExpiresAt = DateTime.Now;
+        HttpClient = new HttpClient();
+    }
+
+    public Client(HttpClient httpClient) {
+        AuthBaseUrl = Constants.AUTH_BASE_URL;
+        BaseUrl = Constants.BASE_URL;
+        Timeout = Constants.API_TIMEOUT;
+        Interval = Constants.POOLING_INTERVAL;
+        Expires = Constants.DEFAULT_EXPIRATION_TIME;
+        ClientID = "";
+        ClientSecret = "";
+        Token = "";
+        AutoRefresh = false;
+        ExpiresAt = DateTime.Now;
+        HttpClient = httpClient;
+    }
+
+    public Client(string clientID, string clientSecret, long expires) {
+        AuthBaseUrl = Constants.AUTH_BASE_URL;
+        BaseUrl = Constants.BASE_URL;
+        Timeout = Constants.API_TIMEOUT;
+        Interval = Constants.POOLING_INTERVAL;
+        Expires = expires;
+        ClientID = clientID;
+        ClientSecret = clientSecret;
+        Token = "";
+        AutoRefresh = true;
+        ExpiresAt = DateTime.Now;
+        HttpClient = new HttpClient();
+    }
+
+    public Client(HttpClient httpClient, string clientID, string clientSecret, long expires) {
+        AuthBaseUrl = Constants.AUTH_BASE_URL;
+        BaseUrl = Constants.BASE_URL;
+        Timeout = Constants.API_TIMEOUT;
+        Interval = Constants.POOLING_INTERVAL;
+        Expires = expires;
+        ClientID = clientID;
+        ClientSecret = clientSecret;
+        Token = "";
+        AutoRefresh = true;
+        ExpiresAt = DateTime.Now;
+        HttpClient = httpClient;
+    }
+
+
+    public void SetAutoRefresh(string clientID, string clientSecret, long expires)
+    {
+        ClientID = clientID;
+        ClientSecret = clientSecret;
+        Expires = expires;
+        AutoRefresh = true;
+        ExpiresAt = DateTime.Now;
+    }
+
+    public void SetBaseUrl(string baseUrl) {
+        BaseUrl = baseUrl;
+    }
+
+    public void SetAuthBaseUrl(string authBaseUrl) {
+        AuthBaseUrl = authBaseUrl;
+    }
+
+    public void SetInterval(long interval) {
+        Interval = interval;
+    }
+
+    public void SetHttpClient(HttpClient httpClient) {
+        HttpClient = httpClient;
+    }
+}
---
+++ ultraocr/Constants.cs
@@ -0,0 +1,25 @@
+namespace ultraocr;
+
+static class Constants
+{
+    public const int STATUS_OK = 200;
+    public const int POOLING_INTERVAL = 1;
+    public const int API_TIMEOUT = 30;
+    public const int DEFAULT_EXPIRATION_TIME = 60;
+    public const string BASE_URL = "https://ultraocr.apis.nuveo.ai/v2";
+    public const string AUTH_BASE_URL = "https://auth.apis.nuveo.ai/v2";
+    public const string STATUS_DONE = "done";
+    public const string STATUS_ERROR = "error";
+    public const string APPLICATION_JSON = "application/json";
+    public const string HEADER_ACCEPT = "Accept";
+    public const string HEADER_CONTENT_TYPE = "Content-Type";
+    public const string HEADER_AUTHORIZATION = "Authorization";
+    public const string BEARER_PREFIX = "Bearer ";
+    public const string KEY_DOCUMENT = "document";
+    public const string KEY_FACEMATCH = "facematch";
+    public const string KEY_EXTRA = "extra-document";
+    public const string KEY_SELFIE = "selfie";
+    public const string KEY_EXTRA_URL = "extra_document";
+    public const string FLAG_TRUE = "true";
+    public const string BASE64_ATTRIBUTE = "base64";
+}
