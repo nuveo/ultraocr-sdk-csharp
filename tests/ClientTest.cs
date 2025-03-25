a81119d3de7b252aa3568b502ed95fb98048c74e
Kleberson Canuto <klebersoncanuto@gmail.com>
2025-03-25 16:37:27 -0300

    Add unit tests

---
+++ tests/ClientTest.cs
@@ -0,0 +1,40 @@
+﻿namespace tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ultraocr;
+
+[TestClass]
+public class ClientTest
+{
+    [TestMethod]
+    public void TestNew()
+    {
+        var client = new Client();
+        Assert.IsNotNull(client);
+        Assert.IsInstanceOfType<Client>(client);
+    }
+
+    [TestMethod]
+    public void TestNewHttp()
+    {
+        var client = new Client(new HttpClient());
+        Assert.IsNotNull(client);
+        Assert.IsInstanceOfType<Client>(client);
+    }
+
+    [TestMethod]
+    public void TestNewAuto()
+    {
+        var client = new Client("123", "123", 60);
+        Assert.IsNotNull(client);
+        Assert.IsInstanceOfType<Client>(client);
+    }
+
+    [TestMethod]
+    public void TestNewFull()
+    {
+        var client = new Client(new HttpClient(), "123", "123", 60);
+        Assert.IsNotNull(client);
+        Assert.IsInstanceOfType<Client>(client);
+    }
+}
